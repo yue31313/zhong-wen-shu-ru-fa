86e35b0283c4ca90b4787901b523707db579a80e
韩亚飞_韩梦飞沙 <313134555@qq.com>
2017-09-16 01:45:55 +0800

    -

---
+++ gen/com/example/contactscall/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.contactscall;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/contactscall/ContactsFragment.java
@@ -0,0 +1,231 @@
+package com.example.contactscall;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.contactscall.adapter.SortAdapter;
+import com.example.contactscall.entity.SortModel;
+import com.example.contactscall.util.CharacterParser;
+import com.example.contactscall.util.ConstactUtil;
+import com.example.contactscall.util.PinyinComparator;
+import com.example.contactscall.view.ClearEditText;
+import com.example.contactscall.view.SideBar;
+import com.example.contactscall.view.SideBar.OnTouchingLetterChangedListener;
+
+/**
+ * 名片夹
+ * 
+ * bug：一个名字只选了第一个电话号码；另外，如果重名的话只显示一个
+ * 
+ * @author WangPeng
+ * 
+ */
+public class ContactsFragment extends Fragment {
+	private Context mContext;
+	private ListView sortListView;
+	private List<SortModel> SourceDateList;
+	private SideBar sideBar;
+	private TextView dialog;
+	private SortAdapter adapter;
+	private ClearEditText mClearEditText;
+	private Map<String, String> callRecords;
+
+	/**
+	 * 汉字转换成拼音的类
+	 */
+	private CharacterParser characterParser;
+
+	/**
+	 * 根据拼音来排列ListView里面的数据类
+	 */
+	private PinyinComparator pinyinComparator;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mContext = getActivity();
+		View rootView = inflater.inflate(R.layout.contacts_fragment, container, false);
+		findView(rootView);
+		init();
+
+		return rootView;
+	}
+
+	private void findView(View rootView) {
+		sideBar = (SideBar) rootView.findViewById(R.id.sidrbar);
+		dialog = (TextView) rootView.findViewById(R.id.dialog);
+		sortListView = (ListView) rootView.findViewById(R.id.country_lvcountry);
+		mClearEditText = (ClearEditText) rootView.findViewById(R.id.filter_edit);
+	}
+
+	private void init() {
+		// 实例化汉字转拼音类
+		characterParser = CharacterParser.getInstance();
+
+		pinyinComparator = new PinyinComparator();
+
+		sideBar.setTextView(dialog);
+
+		// 设置右侧触摸监听
+		sideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener() {
+
+			@SuppressLint("NewApi")
+			@Override
+			public void onTouchingLetterChanged(String s) {
+				try {
+					// 该字母首次出现的位置
+					int position = adapter.getPositionForSection(s.charAt(0));
+					if (position != -1) {
+						sortListView.setSelection(position);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		sortListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// 这里要利用adapter.getItem(position)来获取当前position所对应的对象
+				// Toast.makeText(getApplication(),
+				// ((SortModel)adapter.getItem(position)).getName(),
+				// Toast.LENGTH_SHORT).show();
+				String number = callRecords.get(((SortModel) adapter.getItem(position)).getName());
+				Intent phoneIntent = new Intent("android.intent.action.CALL", Uri.parse("tel:" + number));
+
+				startActivity(phoneIntent);
+			}
+		});
+
+		new AsyncTaskConstact().execute();
+	}
+
+	private class AsyncTaskConstact extends AsyncTask<Integer, Integer, Integer> {
+
+		@Override
+		protected Integer doInBackground(Integer... params) {
+			int result = -1;
+			callRecords = ConstactUtil.getAllCallRecords(mContext);
+			result = 1;
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+			if (result == 1) {
+				List<String> constact = new ArrayList<String>();
+				for (Iterator<String> keys = callRecords.keySet().iterator(); keys.hasNext();) {
+					String key = keys.next();
+					constact.add(key);
+				}
+				String[] names = new String[] {};
+				names = constact.toArray(names);
+				SourceDateList = filledData(names);
+
+				sideBar.setVisibility(View.VISIBLE);
+
+				// 根据a-z进行排序源数据
+				Collections.sort(SourceDateList, pinyinComparator);
+				adapter = new SortAdapter(mContext, SourceDateList);
+				sortListView.setAdapter(adapter);
+
+				// 根据输入框输入值的改变来过滤搜索
+				mClearEditText.addTextChangedListener(new TextWatcher() {
+
+					@Override
+					public void onTextChanged(CharSequence s, int start, int before, int count) {
+						// 当输入框里面的值为空，更新为原来的列表，否则为过滤数据列表
+						filterData(s.toString());
+					}
+
+					@Override
+					public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+					}
+
+					@Override
+					public void afterTextChanged(Editable s) {
+					}
+				});
+			}
+		}
+
+	}
+
+	/**
+	 * 为ListView填充数据
+	 * 
+	 * @param date
+	 * @return
+	 */
+	private List<SortModel> filledData(String[] date) {
+		List<SortModel> mSortList = new ArrayList<SortModel>();
+
+		for (int i = 0; i < date.length; i++) {
+			SortModel sortModel = new SortModel();
+			sortModel.setName(date[i]);
+			// 汉字转换成拼音
+			String pinyin = characterParser.getSelling(date[i]);
+			String sortString = pinyin.substring(0, 1).toUpperCase();
+
+			// 正则表达式，判断首字母是否是英文字母
+			if (sortString.matches("[A-Z]")) {
+				sortModel.setSortLetters(sortString.toUpperCase());
+			} else {
+				sortModel.setSortLetters("#");
+			}
+
+			mSortList.add(sortModel);
+		}
+		return mSortList;
+
+	}
+
+	/**
+	 * 根据输入框中的值来过滤数据并更新ListView
+	 * 
+	 * @param filterStr
+	 */
+	private void filterData(String filterStr) {
+		List<SortModel> filterDateList = new ArrayList<SortModel>();
+
+		if (TextUtils.isEmpty(filterStr)) {
+			filterDateList = SourceDateList;
+		} else {
+			filterDateList.clear();
+			for (SortModel sortModel : SourceDateList) {
+				String name = sortModel.getName();
+				if (name.indexOf(filterStr.toString()) != -1 || characterParser.getSelling(name).startsWith(filterStr.toString())) {
+					filterDateList.add(sortModel);
+				}
+			}
+		}
+
+		// 根据a-z进行排序
+		Collections.sort(filterDateList, pinyinComparator);
+		adapter.updateListView(filterDateList);
+	}
+}
